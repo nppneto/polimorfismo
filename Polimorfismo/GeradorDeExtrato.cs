1d5c1f313f24cc909820eeff6480ecd0c0f7fdae
Nelson Pereira Pinto Neto <neelsoonzd@hotmail.com>
2017-09-21 14:34:00 -0300

    Estudo Polimorfismo

---
+++ Polimorfismo/GeradorDeExtrato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class GeradorDeExtrato
+    {
+        public void ImprimeExtratoBasico(Conta c)
+        {
+            DateTime agora = DateTime.Now;
+            string horario = string.Format("{0:d/M/yyyy HH:mm:ss}", agora);
+
+            Console.WriteLine("DATA: {0:c}", horario);
+            Console.WriteLine("SALDO: {0:c}", c.Saldo);
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ Polimorfismo/TestaGeradorDeExtrato.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class TestaGeradorDeExtrato
+    {
+        static void Main(string[] args)
+        {
+            GeradorDeExtrato gerador = new GeradorDeExtrato();
+
+            ContaPoupanca cp = new ContaPoupanca();
+            cp.Saldo = 2000;
+
+            ContaCorrente cc = new ContaCorrente();
+            cc.Saldo = 1000;
+
+            gerador.ImprimeExtratoBasico(cp);
+            gerador.ImprimeExtratoBasico(cc);
+
+            Console.ReadKey();
+        }
+    }
+}
